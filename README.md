6ecc76b9a9f5d417042f7adf0c63369b348ebd38
Gaurav Kumar <gauravkr748800@gmail.com>
2025-02-06 12:24:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Feature
+- This extension displays the focused word in the current editor window in a informationBox window.
+
+# How to start
+- open the project in a new window.
+- press f5
+- create a new document
+- write something
+- press `ctrl+alt+w`.
---
+++ extension.js
@@ -0,0 +1,30 @@
+const vscode = require('vscode');
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	const disposable = vscode.commands.registerCommand('extension-poc.helloWorld', function () {
+
+		const editor = vscode.window.activeTextEditor;
+		const selection = editor.selection;
+		const wordRange = editor.document.getWordRangeAtPosition(selection.active);
+
+		if (wordRange) {
+			const word = editor.document.getText(wordRange);
+			vscode.window.showInformationMessage(`Focused Word: ${word}`);
+		} else {
+			// If no word is found under the cursor, show a different message.
+			vscode.window.showInformationMessage("No word found under cursor.");
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
